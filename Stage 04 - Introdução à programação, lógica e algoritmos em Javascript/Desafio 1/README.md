16e35ed6915902e66d166cc2b37f4367cce86f5d
RodrigoLuigi <digaor@gmail.com>
2022-05-02 12:20:49 -0300

    Finalizando Desafio 01 - Stage 04

--- Stage 04 - Introdução à programação, lógica e algoritmos em Javascript/Desafio 1/README.md
+++ Stage 04 - Introdução à programação, lógica e algoritmos em Javascript/Desafio 1/README.md
@@ -1,12 +1,12 @@
 Nesse desafio, você irá solicitar ao usuário que ele insira **dois números** e, a partir daí, calcular:
 
-- [ ]  A soma dos dois números;
-- [ ]  A subtração dos dois números;
-- [ ]  A multiplicação dos dois números;
-- [ ]  A divisão dos dois números;
-- [ ]  O resto da divisão dos dois números;
+- [X]  A soma dos dois números;
+- [X]  A subtração dos dois números;
+- [X]  A multiplicação dos dois números;
+- [X]  A divisão dos dois números;
+- [X]  O resto da divisão dos dois números;
 
 Fácil até aqui, né? Bora se desafiar? 👀 🧑‍🚀
 
-- [ ]  Verifique se a soma dos dois números é par ~~(ou ímpar)~~;
-- [ ]  Verifique se os dois números inseridos são iguais ~~(ou diferentes)~~.
+- [X]  Verifique se a soma dos dois números é par ~~(ou ímpar)~~;
+- [X]  Verifique se os dois números inseridos são iguais ~~(ou diferentes)~~.
--- Stage 04 - Introdução à programação, lógica e algoritmos em Javascript/Desafio 1/script.js
+++ Stage 04 - Introdução à programação, lógica e algoritmos em Javascript/Desafio 1/script.js
@@ -1,22 +1,59 @@
+alert("Digite dois números e escolha uma opção no menu de calculos. ")
+
 let firstNumber = Number(prompt("Digite o primeiro número: "));
 let secondNumber = Number(prompt("Digite o segundo número: "));
 
-const sum = firstNumber + secondNumber;
-const sub = firstNumber - secondNumber;
-const mult = firstNumber * secondNumber;
-const div = firstNumber / secondNumber;
-const rest = firstNumber % secondNumber;
+let op;
 
-let op = Number(prompt(`
+while (op != 8) {
+  op = Number(prompt(`
 Digite a opção desejada:  
 
-1.  SOMA: ${firstNumber} + ${secondNumber} = ${sum};
-2.  SUBTRAÇÃO: ${firstNumber} - ${secondNumber} = ${sub};
-3.  MULTIPLICAÇÃO: ${firstNumber} * ${secondNumber} = ${mult};
-4.  DIVISÃO: ${firstNumber} / ${secondNumber} = ${div};
-5.  RESTO DA DIVISÃO: ${firstNumber} % ${secondNumber} = ${rest};
+1.  SOMA
+2.  SUBTRAÇÃO
+3.  MULTIPLICAÇÃO
+4.  DIVISÃO
+5.  RESTO DA DIVISÃO
+6.  VERIFICA SE SOMA È PAR OU IMPAR
+7.  VERIFICA SE SÃO IGUAIS
+8.  Sair
 `));
 
-if(op == 1){
-  
+
+  if (op == 1) {
+    const sum = firstNumber + secondNumber;
+    alert(`SOMA: ${firstNumber} + ${secondNumber} = ${sum}`);
+  } else if (op == 2) {
+    const sub = firstNumber - secondNumber;
+    alert(`SUBTRAÇÃO: ${firstNumber} - ${secondNumber} = ${sub}`);
+  } else if (op == 3) {
+    const mult = firstNumber * secondNumber;
+    alert(`MULTIPLICAÇÃO: ${firstNumber} * ${secondNumber} = ${mult}`);
+  } else if (op == 4) {
+    let div = firstNumber / secondNumber;
+    div = div.toFixed(2);
+    alert(`DIVISÃO: ${firstNumber} / ${secondNumber} = ${div}`);
+  } else if (op == 5) {
+    const rest = firstNumber % secondNumber;
+    alert(`RESTO DA DIVISÃO: ${firstNumber} % ${secondNumber} = ${rest}`);
+  } else if (op == 6) {
+    const sum = firstNumber + secondNumber;
+    const verify = sum % 2;
+    if (verify == 0) {
+      alert(`A soma dos números é Par!`);
+    } else {
+      alert(`A soma dos números é Impar!`);
+    }
+  } else if (op == 7) {
+    if (firstNumber == secondNumber) {
+      alert("Os números são iguais!")
+    } else {
+      alert("Os números são diferentes!")
+    }
+  } else if (op == 8) {
+    alert("Encerrando aplicação . . .");
+  } else {
+    alert(`Opção inválida!`);
+  }
+
 }
