399a828f6f5b7ff4a76bfc8db10c4b57b4a63ead
RodrigoLuigi <digaor@gmail.com>
2022-06-01 18:12:19 -0300

    Finalizando Projeto 2

--- Stage 06 - JavaScript antes do framework/Projeto2/js/Favorites.js
+++ Stage 06 - JavaScript antes do framework/Projeto2/js/Favorites.js
@@ -1,22 +1,4 @@
-export class GithubUser {
-  static search(username) {
-    const endpoint = `https://api.github.com/users/${username}`
-
-    return fetch(endpoint)
-      .then(data => data.json())
-      .then(({
-        login,
-        name,
-        public_repos,
-        followers
-      }) => ({
-        login,
-        name,
-        public_repos,
-        followers
-      }))
-  }
-}
+import { GithubUser } from "./GithubUser.js"
 
 // classe que vai conter a lógica dos dados
 // como os dados serão estruturados
@@ -27,13 +9,33 @@ export class Favorites {
   }
 
   load() {
-    this.entries = JSON.parse(localStorage.getItem('@github-favorites')) || []
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+  }
+
+  save(){
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
   }
 
   async add(username){
-     const user = await GithubUser.search(username)
+    try{
+      const userExists = this.entries.find(entry => entry.login === username)
 
-     console.log(user)
+      if(userExists) {
+        throw new Error('Usuário já cadastrado!')
+      }
+
+      const user = await GithubUser.search(username)
+
+      if(user.login === undefined){
+        throw new Error('Usuário não encontrado')
+      }
+
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+    }catch(error){
+      alert(error.message)
+    }
   }
 
   delete(user) {
@@ -42,6 +44,7 @@ export class Favorites {
 
     this.entries = filteredEntries
     this.update();
+    this.save()
   }
 
 }
@@ -75,7 +78,9 @@ export class FavoritesView extends Favorites {
 
       row.querySelector('.user img').src = `https://github.com/${user.login}.png`
       row.querySelector('.user img').alt = `Image de ${user.name}`
+      row.querySelector('.user a').href = `https://github.com/${user.login}`
       row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
       row.querySelector('.repositories').textContent = user.public_repos
       row.querySelector('.followers').textContent = user.followers;
 
