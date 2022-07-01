3339b1e704c238bd5cc0cc79cc8a6fde1b905f1f
RodrigoLuigi <digaor@gmail.com>
2022-06-30 23:57:17 -0300

    Create project  1 - Node.js

---
+++ Stage 08 - Backend/Projeto1/src/server.js
@@ -0,0 +1,22 @@
+const { response } = require("express");
+const express = require("express");
+
+const app = express();
+
+app.get("/message/:id/:user", (request,response) => {
+  const {id, user} = request.params;
+  response.send(`
+    Mensagem ID: ${id}
+    Para o usuário: ${user}
+    `);
+});
+
+app.get("/users" , (request,response)=>{
+  const {page, limit} = request.query;
+
+  response.send(`Página: ${page}. Mostrar: ${limit}`);
+})
+
+const PORT = 3333;
+
+app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
