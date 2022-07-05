662b2c4285348dfe75ae9e0a8172bd2e71629aea
RodrigoLuigi <digaor@gmail.com>
2022-07-05 17:40:47 -0300

    Create Data Base

--- Stage 08 - Backend/Projeto1/src/server.js
+++ Stage 08 - Backend/Projeto1/src/server.js
@@ -1,5 +1,7 @@
 require("express-async-errors");
 
+const database = require("./database/sqlite")
+
 const AppError = require("./utils/AppError")
 const express = require("express");
 
@@ -9,6 +11,8 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+database();
 
 app.use((error, request, response, next) => {
   if(error instanceof AppError){
