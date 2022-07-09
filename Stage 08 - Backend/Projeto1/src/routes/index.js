a9af2a68e7c2031c19f43c2cbae8c1ff9f5f8782
RodrigoLuigi <digaor@gmail.com>
2022-07-08 21:59:25 -0300

    Finalizando projeto backend

--- Stage 08 - Backend/Projeto1/src/routes/index.js
+++ Stage 08 - Backend/Projeto1/src/routes/index.js
@@ -3,9 +3,13 @@ const {
 } = require('express');
 
 const userRouter = require("./users.routes");
+const notesRouter = require("./notes.routes");
+const tagsRouter = require("./tags.routes");
 
 const routes = Router();
 
 routes.use("/users", userRouter);
+routes.use("/notes", notesRouter);
+routes.use("/tags", tagsRouter);
 
 module.exports = routes
---
+++ Stage 08 - Backend/Projeto1/src/routes/tags.routes.js
@@ -0,0 +1,13 @@
+const {
+  Router
+} = require("express");
+
+const TagsController = require('../controllers/TagsController');
+
+const tagsController = new TagsController();
+
+const tagsRoutes = Router();
+
+tagsRoutes.get("/:user_id", tagsController.index);
+
+module.exports = tagsRoutes;
