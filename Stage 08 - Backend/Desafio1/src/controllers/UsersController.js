830db35df0bb496c762714b64d23183456d5e9f0
RodrigoLuigi <digaor@gmail.com>
2022-07-11 16:03:12 -0300

    Create migrations , create user in DB, user existis validadion

--- Stage 08 - Backend/Desafio1/src/controllers/UsersController.js
+++ Stage 08 - Backend/Desafio1/src/controllers/UsersController.js
@@ -1,14 +1,28 @@
 const AppError = require("../utils/AppError");
 
-class UsersController{
-  create(request, response){
-    const { name, email, password} = request.body;
+const sqliteConnection = require("../database/sqlite");
 
-    if(!name){
-      throw new AppError('Nome é obrigatório');
+class UsersController {
+  async create(request, response) {
+    const {
+      name,
+      email,
+      password
+    } = request.body;
+
+    const database = await sqliteConnection();
+
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+    if (checkUserExists) {
+      throw new AppError("Este e-mail já está em uso.");
     }
 
-    response.status(201).json({name, email, password});
+    await database.run(
+      "INSERT INTO users(name, email, password) VALUES(?, ?, ?)",
+      [name, email, password]
+    );
+
+    return response.status(201).json();
   }
 
 }
