7a0d7f230c6ce4b2caccdc40278dcbae71810389
RodrigoLuigi <digaor@gmail.com>
2022-07-18 20:55:25 -0300

    Finalizando  a Home

--- Stage 09 - Frontend/LP/script.js
+++ Stage 09 - Frontend/LP/script.js
@@ -12,12 +12,15 @@ for (const element of toggle) {
 const header = document.querySelector('#header');
 const navHeight = header.offsetHeight;
 
-
 function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll');
+    header.style.background = 'black';
+    header.style.borderBottom = '1px solid white';
   } else {
     header.classList.remove('scroll');
+    header.style.background = 'none';
+    header.style.borderBottom = 'none';
   }
 }
 
