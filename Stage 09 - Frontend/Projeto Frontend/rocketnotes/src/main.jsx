f78109fdcb523adb743d7f03ccd1d49a47715962
digaor <digaor@gmail.com>
2022-08-12 16:40:26 -0300

    Finalizando page SignUp

--- Stage 09 - Frontend/Projeto Frontend/rocketnotes/src/main.jsx
+++ Stage 09 - Frontend/Projeto Frontend/rocketnotes/src/main.jsx
@@ -6,14 +6,14 @@ import GlobalStyles from './styles/global';
 import theme from './styles/theme'
 
 /* import { Details } from './pages/Details'; */
-import { SignIn } from './pages/SignIn';
+import { SignUp } from './pages/SignUp';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ Stage 09 - Frontend/Projeto Frontend/rocketnotes/src/pages/SignUp/index.jsx
@@ -0,0 +1,42 @@
+import { Container, Form, Background } from './styles';
+import { FiMail, FiLock, FiUser } from 'react-icons/fi'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+export function SignUp(){
+  return(
+    <Container>
+      <Background />
+
+
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+        <h2>Crie sua conta</h2>
+
+        <Input 
+        placeholder="Nome"
+        type="text"
+        icon={FiUser}
+        />
+        <Input 
+        placeholder="E-mail"
+        type="text"
+        icon={FiMail}
+        />
+        <Input 
+        placeholder="Senha"
+        type="password"
+        icon={FiLock}
+        />
+
+        <Button title="Cadastrar" />
+
+        <a href="#">Voltar para o login</a>
+
+      </Form>
+    </Container>
+  );
+}
