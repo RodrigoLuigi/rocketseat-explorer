0466ca80b2252d954676c19675c519940340b2d0
digaor <digaor@gmail.com>
2022-08-12 16:14:33 -0300

    Finalizando page SignIn

--- Stage 09 - Frontend/Projeto Frontend/rocketnotes/src/main.jsx
+++ Stage 09 - Frontend/Projeto Frontend/rocketnotes/src/main.jsx
@@ -6,14 +6,14 @@ import GlobalStyles from './styles/global';
 import theme from './styles/theme'
 
 /* import { Details } from './pages/Details'; */
-import { Home } from './pages/Home';
+import { SignIn } from './pages/SignIn';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>
 )
