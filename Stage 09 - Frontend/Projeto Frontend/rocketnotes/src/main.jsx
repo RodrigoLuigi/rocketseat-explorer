147552791341a9020ee8a672528331c4eacf7623
digaor <digaor@gmail.com>
2022-08-13 16:51:29 -0300

    Finalizando page NewNote, create components NoteItem/Textarea

--- Stage 09 - Frontend/Projeto Frontend/rocketnotes/src/main.jsx
+++ Stage 09 - Frontend/Projeto Frontend/rocketnotes/src/main.jsx
@@ -6,14 +6,14 @@ import GlobalStyles from './styles/global';
 import theme from './styles/theme'
 
 /* import { Details } from './pages/Details'; */
-import { Profile } from './pages/Profile';
+import { NewNote } from './pages/NewNote';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Profile />
+      <NewNote />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ Stage 09 - Frontend/Projeto Frontend/rocketnotes/src/pages/NewNote/index.jsx
@@ -0,0 +1,44 @@
+import { Textarea } from '../../components/Textarea';
+import { NoteItem } from '../../components/NoteItem';
+import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+
+
+import { Container, Form } from './styles';
+
+export function NewNote(){
+  return(
+    <Container>
+      <Header />
+
+      <main>
+      <Form>
+        <header>
+          <h1>Criar nota</h1>
+          <a href="/">Voltar</a>
+        </header>
+
+        <Input placeholder="Título" />
+        <Textarea placeholder="Observações" />
+
+        <Section title="Links úteis">
+          <NoteItem value="https://rocketseat.com.br"/>
+          <NoteItem isNew placeholder="Novo link"/>
+        </Section>
+
+        <Section title="Marcadores">
+        <div className='tags'>
+          <NoteItem value="react" />
+          <NoteItem isNew placeholder="Nova tag" />
+        </div>
+        </Section>
+
+        <Button title="Salvar" />          
+        
+      </Form>
+      </main>
+    </Container>
+  );
+}
