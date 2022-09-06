8eda57f3f1d719d509135a8b71b9f8bd52d6ef77
RodrigoLuigi <digaor@gmail.com>
2022-09-06 09:23:19 -0300

    create: app.routes / auth.routes in routes

--- Stage 09 - Frontend/rocket-movies/src/main.jsx
+++ Stage 09 - Frontend/rocket-movies/src/main.jsx
@@ -5,13 +5,13 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import GlobalStyles from './styles/global';
 
-import { SignIn } from './pages/SignIn'
+import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignIn />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
