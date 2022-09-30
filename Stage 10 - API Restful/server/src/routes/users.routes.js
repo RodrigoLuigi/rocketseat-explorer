1ccf19736b881aed08bcf2bb36b75bac92501729
RodrigoLuigi <digaor@gmail.com>
2022-09-30 14:04:17 -0300

    feat: create UserAvatarController.js

--- Stage 10 - API Restful/server/src/routes/users.routes.js
+++ Stage 10 - API Restful/server/src/routes/users.routes.js
@@ -1,16 +1,20 @@
-const {
-  Router
-} = require("express");
+const { Router } = require("express");
+const multer = require("multer");
+const uploadConifg = require("../configs/upload");
 
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
+const UserAvatarController = require("../controllers/UserAvatarController");
 const UsersController = require('../controllers/UsersController');
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 const userRoutes = Router();
+const upload = multer(uploadConifg.MULTER);
 
 userRoutes.post("/", usersController.create);
 userRoutes.put("/", ensureAuthenticated, usersController.update);
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 module.exports = userRoutes;
