b893d30dc60878a00031ee833a53dfb44da69133
RodrigoLuigi <digaor@gmail.com>
2022-09-30 14:15:11 -0300

    env: add app.use route /file in server.js

--- Stage 10 - API Restful/server/src/server.js
+++ Stage 10 - API Restful/server/src/server.js
@@ -1,6 +1,7 @@
 require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
+const uploadConfig = require("./configs/upload");
 
 const express = require("express");
 
@@ -11,6 +12,7 @@ migrationsRun();
 const app = express();
 app.use(express.json());
 
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(routes);
 
 app.use((error, request, response, next) => {
