2d14dc9da629adf1e2275eb95f03f2487b2e9e91
RodrigoLuigi <digaor@gmail.com>
2022-10-11 16:04:53 -0300

    fix: adjust notes and tags order

--- Stage 10 - API Restful/server/src/controllers/NotesController.js
+++ Stage 10 - API Restful/server/src/controllers/NotesController.js
@@ -75,7 +75,8 @@ class NotesController{
       .whereLike("notes.title", `%${title}%`)
       .whereIn("name", filterTags)
       .innerJoin("notes", "notes.id", "tags.note_id")
-      .orderBy("notes.title");
+      .groupBy("notes.id")
+      .orderBy("notes.title")
     }else{
       notes = await knex("notes")
       .where({ user_id })
--- Stage 10 - API Restful/server/src/controllers/TagsController.js
+++ Stage 10 - API Restful/server/src/controllers/TagsController.js
@@ -5,7 +5,8 @@ class TagsController {
     const user_id = request.user.id;
 
     const tags = await knex("tags")
-    .where({ user_id });
+    .where({ user_id })
+    .groupBy("name")
 
     return response.json(tags);
   }
