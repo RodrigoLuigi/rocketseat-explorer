2d370b1b28f5285afa15074cca6c88026d483cdd
RodrigoLuigi <digaor@gmail.com>
2022-10-13 11:38:17 -0300

    feat: binding JWT token to user

--- Stage 10 - API Restful/Rocket-Movies/server/src/controllers/SessionsController.js
+++ Stage 10 - API Restful/Rocket-Movies/server/src/controllers/SessionsController.js
@@ -1,5 +1,7 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
+const authConfig = require("../configs/auth");
+const { sign } = require("jsonwebtoken");
 
 //função do bcrypt utilizamos para fazer a comparação de senhas
 const { compare } = require("bcryptjs");
@@ -26,7 +28,15 @@ class SessionsController {
       throw new AppError("E-mail e/ou senha incorreta", 401);
     }
 
-    return response.json(user);
+    //trazendo as informações do JWT de forma desestruturada
+    const { secret, expiresIn } = authConfig.jwt;
+    //cria o token de fato, utilizando o método sign() passando um objeto vazio que no caso está configurado como default em nossa configs e um subject que são as informações que queremos passar junto com o token, no caso o "id" do usuário.
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
+
+    return response.json({user, token});
   }
 }
 
