8e722fe03dd870e657de9b67ee185b10dcd90c8e
RodrigoLuigi <digaor@gmail.com>
2022-10-18 10:53:40 -0300

    feat: create hook auth / function handleSignIn

--- Stage 10 - API Restful/Rocket-Movies/web/src/main.jsx
+++ Stage 10 - API Restful/Rocket-Movies/web/src/main.jsx
@@ -5,13 +5,17 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import GlobalStyles from './styles/global';
 
+import { AuthProvider } from './hooks/auth';
+
 import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
