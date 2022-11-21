b0f262eaebcf736f70e4c10b15bde4fb2adad14e
RodrigoLuigi <digaor@gmail.com>
2022-11-21 19:38:50 -0300

    env: install pm22 and ecosystem.configs

---
+++ Stage 10 - API Restful/Rocket-Movies/server/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [{
+    name: "app",
+    script: "./src/server.js",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
+}
